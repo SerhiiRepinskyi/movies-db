207e459c60e2511768ab4abb4f86e8ee79faadff
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2024-04-17 00:29:50 +0300

    feat: added video player, used useRef to start and stop video

--- src/features/About/About.tsx
+++ src/features/About/About.tsx
@@ -1,10 +1,12 @@
 import { Container } from "@mui/material";
+import { CountdownVideo } from "./CountdownVideo";
 import { CountdownText } from "./CountdownText";
 
 function About() {
   return (
     <Container sx={{ py: 8 }} maxWidth="md">
       <CountdownText />
+      <CountdownVideo />
     </Container>
   );
 }
