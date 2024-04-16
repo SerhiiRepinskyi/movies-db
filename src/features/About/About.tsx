8cad3dae0785010e1eabc9ab0c6afe473e89d43d
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2024-04-17 00:11:58 +0300

    feat: added a countdown timer using useRef

--- src/features/About/About.tsx
+++ src/features/About/About.tsx
@@ -1,5 +1,13 @@
+import { Container } from "@mui/material";
+import { CountdownText } from "./CountdownText";
+
 function About() {
-  return <div>Comming soon...</div>;
+  return (
+    <Container sx={{ py: 8 }} maxWidth="md">
+      <CountdownText />
+    </Container>
+  );
 }
 
 export default About;
+
